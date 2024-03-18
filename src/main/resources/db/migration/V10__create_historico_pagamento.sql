7788232957ad8bbd29d592e8af22b3e71dc432ca
gleniomontovani <gleniomontovani@gmail.com>
2024-03-18 07:00:48 -0300

    Esteira

--- src/main/resources/db/migration/V10__create_historico_pagamento.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS historico_pagamento (
-                                      id INTEGER PRIMARY KEY,
-                                      pagamento_id INTEGER,
-                                      data_historico TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                      data_pagamento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                      numero_tentativas INTEGER,
-                                      descricao VARCHAR(255),
-                                      FOREIGN KEY (pagamento_id) REFERENCES pagamento (id)
-);
